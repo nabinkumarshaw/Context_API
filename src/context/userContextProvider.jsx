cd5d58f13ce97759ae6f4a42f09f2c1c8f638a4a
Nabin Kumar Shaw <122195307+nabinkumarshaw@users.noreply.github.com>
2025-07-26 11:32:27 +0530

    Update userContextProvider.jsx

--- src/context/userContextProvider.jsx
+++ src/context/userContextProvider.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import usercontext from "./userContext";
 
 const usercontextprovider = ({children})=>{
-    const [user,setUser] = useState(null)
+    const [user,setUser] = useState(null)  //ye user aur setuser kisi component mey pass hga
+    
     return(
         <usercontext.Provider value={{user,setUser}}>
         {children}
